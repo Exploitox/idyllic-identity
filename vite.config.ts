b570d6ee2df025510ecbb10da361e1f40c9addee
Jonas Günner <jonas@exploitox.de>
2025-05-14 22:45:26 +0200

    Keycloakify Template

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { keycloakify } from 'keycloakify/vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    tailwindcss(),
+    keycloakify({
+      themeName: 'keycloakify-starter-svelte',
+      accountThemeImplementation: 'none',
+    }),
+  ],
+});
